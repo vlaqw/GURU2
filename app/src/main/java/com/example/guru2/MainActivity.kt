7689133c0a2c22cf3de5179054ba760644dfe423
vlaqw <128676694+vlaqw@users.noreply.github.com>
2025-01-24 15:01:59 +0900

    35

--- app/src/main/java/com/example/guru2/MainActivity.kt
+++ app/src/main/java/com/example/guru2/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.guru2
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import com.example.guru2.ItemMain
+import com.example.guru2.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonFragment: Button
+    private lateinit var buttonItemMain: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        buttonFragment = findViewById(R.id.btn_manage_items) //버튼 ID
+        buttonFragment.setOnClickListener {
+            val fragment = ItemFragment()//프래그먼트 생성
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment) //컨테이너에 프래먼트 추가
+                .addToBackStack(null) // BackStack에 추가하여 뒤로가기가 가능하도록 설정
+                .commit()
+        }
+
+
+        buttonItemMain = findViewById(R.id.btn_open_item_page)
+        buttonItemMain.setOnClickListener {
+            val intent = Intent(this, ItemMain::class.java)
+            startActivity(intent)
         }
     }
 }
