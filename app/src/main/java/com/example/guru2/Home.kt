8bf17d4d9bf7a8c06cb63b6869d41035d375453d
Chea-yunzi <katete051@naver.com>
2025-02-01 21:01:13 +0900

    기능적 오류 해결 + 앞 페이지 로고 추가

--- app/src/main/java/com/example/guru2/Home.kt
+++ app/src/main/java/com/example/guru2/Home.kt
@@ -18,9 +18,8 @@ import androidx.appcompat.app.AppCompatActivity // 추가
 import androidx.fragment.app.Fragment // 추가
 
 
-class Home : AppCompatActivity() {
-    private val jdbcUrl = "jdbc:mysql://192.168.219.101:3306/check_list_db"
-    //private val jdbcUrl = "jdbc:mysql://192.168.45.85:3306/check_list_db?useUnicode=true&characterEncoding=utf8mb4"
+class Home : ComponentActivity() {
+    private val jdbcUrl = "jdbc:mysql://192.168.45.85:3306/check_list_db"
     private val dbUser = "root"
     private val dbPassword = "123456"
 
